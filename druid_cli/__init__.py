7bec806554f9c64b46db967b8ab366ff0f5128fd
Michał Jaworski <m.jaworski@clearcode.cc>
2014-12-01 13:51:33 +0100

    let's pretend this never happened...

--- druid_cli/__init__.py
+++ druid_cli/__init__.py
@@ -8,13 +8,13 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# ianitor is distributed in the hope that it will be useful,
+# druid-cli is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU Lesser General Public License
-# along with ianitor.  If not, see <http://www.gnu.org/licenses/>.
+# along with druid-cli.  If not, see <http://www.gnu.org/licenses/>.
 
 # flake8: noqa
 VERSION = (0, 0, 1)  # PEP 386
